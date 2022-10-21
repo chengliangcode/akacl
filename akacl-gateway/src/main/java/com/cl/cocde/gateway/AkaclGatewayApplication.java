47d84ed46fb457884400b67904e5a0c2de60204d
charlie <71552682@qq.com>
2022-10-22 01:41:49 +0800

    init

---
+++ akacl-gateway/src/main/java/com/cl/cocde/gateway/AkaclGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.cl.cocde.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chengliang
+ * @since 2022/10/22 1:38
+ */
+
+@SpringBootApplication
+public class AkaclGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkaclGatewayApplication.class, args);
+    }
+
+}
